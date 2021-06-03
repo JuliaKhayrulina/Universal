2d3e110ee79ffb0bbf996b4c9078d6e39bd4a129
Julia Khayrulina <julia748@mail.ru>
2021-06-03 11:21:12 +0300

    create: mobile menu and responsive

--- js/app.js
+++ js/app.js
@@ -2,7 +2,6 @@ document.addEventListener('DOMContentLoaded', () => {
   'use strict';
 
   //==============tabs===============//
-
   const tabs = document.querySelectorAll('.recommend-tabs__item');
   const contents = document.querySelectorAll('.recommend-content');
   for (let tab of tabs) {
@@ -13,11 +12,23 @@ document.addEventListener('DOMContentLoaded', () => {
     for (let content of contents) {
       content.classList.remove('recommend-content--show');
     }
-    if (window.screen.width > 767) {
+    if (window.screen.width > 576) {
       const tab = this;
       const tabAttr = tab.getAttribute('data-tab');
       const content = document.getElementById(`${tabAttr}`);
       content.classList.add('recommend-content--show');
     }
   }
+  //==========mobile menu================//
+  let menuBtn = document.querySelector('.menu-button');
+  let menu = document.querySelector('.menu');
+  let menuLines = document.querySelectorAll('.menu-button__line');
+
+  menuBtn.addEventListener('click', function () {
+    menuLines.forEach((item) => {
+      item.classList.toggle('menu-button__line--active');
+    });
+
+    menu.classList.toggle('menu--visible');
+  });
 });
